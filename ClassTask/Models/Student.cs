b14006774848abaf83a16cba21dc2c0a179c482b
LemanMammad <101627982+LemanMammadly@users.noreply.github.com>
2023-04-24 23:21:24 +0400

    task

---
+++ ClassTask/Models/Student.cs
@@ -0,0 +1,55 @@
+﻿public class Student
+{
+	public string Name;
+	public string Surname;
+	public string Group;
+	public double Point;
+	public bool isGraduated;
+
+
+	public Student(string name, string surname, string group, double point, bool isgraduated)
+	{
+		Name = name;
+		Surname = surname;
+		Group = group;
+		Point = point;
+		isGraduated = isgraduated;
+	}
+
+
+	public string FullName()
+	{
+		return $"{Name} {Surname}";
+	}
+
+
+	public string Info()
+	{
+
+        if (isGraduated)
+        {
+            Console.WriteLine("Telebe mezun olmuşdur.");
+        }
+        else
+        {
+            Console.WriteLine("Telebe mezun olmamışdır.");
+        }
+        return $"Name : {Name} Surname: {Surname} Group: {Group} Point: {Point} isGraduated: {isGraduated}";
+
+	}
+
+
+	public void NextExam()
+	{
+		if(Point>80)
+		{
+		    Console.WriteLine("Telebe növbəti imtahana daxil ola biler.");
+		}
+		else {
+			Console.WriteLine("Telebenin növbəti imtahan daxil olması üçün balı çatmır.");
+		}
+	}
+
+}
+
+
---
+++ ClassTask/Program.cs
@@ -0,0 +1,5 @@
+﻿Student stu1 = new Student("Laman", "Mammadli", "P137", 100, true);
+
+Console.WriteLine(stu1.FullName());
+Console.WriteLine(stu1.Info());
+stu1.NextExam();
